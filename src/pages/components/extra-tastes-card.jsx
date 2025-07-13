c97d3faad0fd5686dfc16fd5cb6c1fca232fd370
CarlaRochaBovi <carlarochabovi08@gmail.com>
2025-07-13 18:11:32 -0300

    website images correction

--- src/pages/components/extra-tastes-card.jsx
+++ src/pages/components/extra-tastes-card.jsx
@@ -5,13 +5,13 @@ export default function ExtraTastesCard() {
          <div className="flex flex-col items-center bg-white p-4 rounded-lg shadow-sm border border-zinc-100">
             <h2 className="text-2xl max-sm:text-xl font-bold text-indigo-700 mb-2">Um hobby:</h2>
             <p className="text-lg max-sm:text-md text-zinc-800 mb-4 text-center">Jogar videogame</p>
-            <img src="./src/assets/gamepad.png" alt="Jogar videogame" className="w-40 h-40 object-cover max-sm:w-30 max-sm:h-30" />
+            <img src="./assets/gamepad.png" alt="Jogar videogame" className="w-40 h-40 object-cover max-sm:w-30 max-sm:h-30" />
          </div>
 
          <div className="flex flex-col items-center bg-white p-4 rounded-lg shadow-sm border border-zinc-100">
             <h2 className="text-2xl max-sm:text-xl font-bold text-indigo-700 mb-2">Uma curiosidade:</h2>
             <p className="text-lg max-sm:text-md text-zinc-800 mb-4 text-center">Gosto de resolver cubos mágicos!</p>
-            <img src="./src/assets/rubiks-cube.png" alt="Cubos mágicos" className="w-40 h-40 object-cover max-sm:w-30 max-sm:h-30" />
+            <img src="./assets/rubiks-cube.png" alt="Cubos mágicos" className="w-40 h-40 object-cover max-sm:w-30 max-sm:h-30" />
          </div>
 
       </div>
